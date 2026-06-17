53693f8ff72757ba493003d821fbae1767ce927b
Gabriel-Androeni <gabriel.andreoni@hotmail.com>
2026-06-17 15:01:41 -0300

    Criação da rota POST

---
+++ demo/src/main/java/com/example/api/Server.java
@@ -0,0 +1,22 @@
+package com.example.api;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.example.handlers.WorkersHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        // 1. Cria o servidor na porta 8080
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        // 2. Associa o endpoint "/produtos" ao nosso Handler
+        server.createContext("/workers", new WorkersHandler());
+
+        // 3. Inicializa o servidor
+        server.setExecutor(null); // Usa o executor padrão
+        System.out.println("Servidor rodando puro na porta 8080...");
+        server.start();
+    }
+}
---
+++ demo/src/main/java/com/example/handlers/WorkersHandler.java
@@ -0,0 +1,46 @@
+package com.example.handlers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class WorkersHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        if ("POST".equalsIgnoreCase(exchange.getRequestMethod())) {
+            InputStream is = exchange.getRequestBody();
+
+            String body = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+
+            System.out.println("JSON recebido: " + body);
+
+            String jsonResponse = """
+                    {
+                        "status": "sucesso",
+                        "mensagem": "Colaborador cadastrado com sucesso!"            
+                    }
+                    """;
+
+            exchange.getResponseHeaders().set("Content-Type", "application/json; charset=UTF-8");
+
+            byte[] responseBytes = jsonResponse.getBytes(StandardCharsets.UTF_8);
+            exchange.sendResponseHeaders(201, responseBytes.length);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(responseBytes);
+            os.close();
+        } else {
+            String erro = "Método não permitido. Use POST!";
+            exchange.sendResponseHeaders(405, erro.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(erro.getBytes());
+            os.close();
+        }
+    }
+}
