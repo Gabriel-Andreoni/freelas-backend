1939dd1586616dab29348ff8acbd4d9c2f720f81
Gabriel-Androeni <gabriel.andreoni@hotmail.com>
2026-06-18 13:30:07 -0300

    Criação da Classe GetRequest que implementa a Interface Handle

---
+++ demo/src/main/java/com/example/handlers/GetRequest.java
@@ -0,0 +1,9 @@
+package com.example.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+
+public class GetRequest implements Handle {
+    public void handleRequest(HttpExchange exchange) {
+        
+    }
+}
