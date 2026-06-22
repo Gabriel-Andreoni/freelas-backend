021ba90b9804d6b9df73bca2d6a1d697d9910334
Gabriel-Androeni <gabriel.andreoni@hotmail.com>
2026-06-22 09:56:07 -0300

    Implementação do método POST da rota contracts

--- demo/src/main/java/com/example/handlers/ContractsPostRequest.java
+++ demo/src/main/java/com/example/handlers/ContractsPostRequest.java
@@ -1,11 +1,34 @@
 package com.example.handlers;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 import com.sun.net.httpserver.HttpExchange;
 
 public class ContractsPostRequest implements Handle {
+    
+    @Override
     public void handleRequest(HttpExchange exchange) throws IOException {
+        InputStream is = exchange.getRequestBody();
+
+        String body = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+
+        System.out.println("Body recebido: " + body);
+
+        String jsonResponse = """
+                {
+                    "status": "sucesso",
+                    "mensagem": "Contrato cadastrado com sucesso!"
+                }
+                """;
         
+        byte[] responseBytes = jsonResponse.getBytes(StandardCharsets.UTF_8);
+        exchange.sendResponseHeaders(201, responseBytes.length);
+
+        OutputStream os = exchange.getResponseBody();
+        os.write(responseBytes);
+        os.close();
     }
 }
